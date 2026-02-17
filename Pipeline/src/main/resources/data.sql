6cf1b5ed4f0b7b95ae73036bd8259653fe1dfb31
Luis-Pozo-Benz <lpozobenz@cifpfbmoll.eu>
2026-02-17 11:53:50 +0100

    Create data.sql

---
+++ Pipeline/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO personas(nombre, talla_pie) VALUES ('Juan', 42);
+INSERT INTO personas(nombre, talla_pie) VALUES ('Ana', 38);
+
+INSERT INTO zapatos(marca, precio) VALUES ('Nike', 100);
+INSERT INTO zapatos(marca, precio) VALUES ('Adidas', 80);
