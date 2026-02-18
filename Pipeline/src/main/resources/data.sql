cae77228ca26c242093698b74163678b3958f93b
Luis-Pozo-Benz <lpozobenz@cifpfbmoll.eu>
2026-02-18 16:11:49 +0100

    Add more fixtures

--- Pipeline/src/main/resources/data.sql
+++ Pipeline/src/main/resources/data.sql
@@ -1,5 +1,11 @@
 INSERT INTO personas(nombre, talla_pie) VALUES ('Juan', 42);
 INSERT INTO personas(nombre, talla_pie) VALUES ('Ana', 38);
+INSERT INTO personas(nombre, talla_pie) VALUES ('Pedro', 40);
+INSERT INTO personas(nombre, talla_pie) VALUES ('Luis', 44);
+INSERT INTO personas(nombre, talla_pie) VALUES ('Erik', 32);
 
 INSERT INTO zapatos(marca, precio) VALUES ('Nike', 100);
 INSERT INTO zapatos(marca, precio) VALUES ('Adidas', 80);
+INSERT INTO zapatos(marca, precio) VALUES ('Jordan', 200);
+INSERT INTO zapatos(marca, precio) VALUES ('Asics', 20);
+INSERT INTO zapatos(marca, precio) VALUES ('Puma', 40);
